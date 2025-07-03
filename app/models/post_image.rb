dc1802b4eef1535dd51ca0d03d265a272291a81d
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-03 13:43:59 +0000

    [Finish] 4-18

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -5,6 +5,7 @@ class PostImage < ApplicationRecord
   validates :caption, presence: true
   
   belongs_to :user
+  has_many :post_comments, dependent: :destroy
 
   def get_image
     unless image.attached?
