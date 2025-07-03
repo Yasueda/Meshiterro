b0b9379e715526a07af5fde5081bb9c50aab3f2f
Yasueda <217292364+Yasueda@users.noreply.github.com>
2025-07-03 07:10:10 +0000

    [Finish] 4-14

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
+
+  resources :users, only: [:show, :edit]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
